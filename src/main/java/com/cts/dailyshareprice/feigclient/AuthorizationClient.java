68e247f92bc6fd33d67123ab8eedd4f859ff874e
shakil <shakilahamed0607@gmail.com>
2022-07-04 22:32:05 +0530

    completed microservice

--- src/main/java/com/cts/dailyshareprice/feigclient/AuthorizationClient.java
+++ src/main/java/com/cts/dailyshareprice/feigclient/AuthorizationClient.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(url = "http://localhost:8081", name = "AUTHORIZATION-SERVICE")
+@FeignClient(url = "http://ij007pod2authorization-env.eba-fsjdukpv.eu-central-1.elasticbeanstalk.com:8081", name = "AUTHORIZATION-SERVICE")
+//@FeignClient(url = "http://localhost:8081", name= "Authorization-Service")
 public interface AuthorizationClient {
 	
 	@GetMapping("/getuname")
